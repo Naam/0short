a4ec924bc52a352631de01eb4054937662cb74c9
El Atmani Nahim <nahim.elatmani@epita.fr>
2014-05-04 02:58:56 +0200

    finishing crypto module

--- murl/crypto.py
+++ murl/crypto.py
@@ -4,10 +4,9 @@ from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
 from binascii import hexlify, unhexlify
 
-iterations  = 14253
-padding_size= 16
-
-plaintext   = "Big brother is watching you"
+iterations      = 14253
+padding_size    = 16
+iv_size         = 16
 
 pad = lambda s: s + (padding_size - len(s) % padding_size) *\
         chr(padding_size - len(s) % padding_size)
@@ -18,14 +17,13 @@ padding = lambda s : s + (padding_size - len(s) % padding_size)*\
 def encrypt(text):
     salt        = get_random_bytes(16)
     passwd      = get_random_bytes(32)
-    iv          = get_random_bytes(16)
+    iv          = get_random_bytes(iv_size)
     key         = PBKDF2(passwd, salt, dkLen=32, count=iterations)
     cipher      = AES.new(key, AES.MODE_CBC, iv)
-    return (iv + cipher.encrypt(pad(text)))
+    return (hexlify(iv + cipher.encrypt(pad(text))), key)
 
-def decrypt(ciphered, key, iv):
-    return unpad(AES.new(key, AES.MODE_CBC, ciphered[:len(iv)]).decrypt(ciphered[len(iv):]))
-print (hexlify(encrypt(plaintext)))
-print (plaintext)
-
-
+def decrypt(ciphered, key):
+    data        = unhexlify(ciphered)
+    iv          = data[:iv_size]
+    ciphered    = data[iv_size:]
+    return unpad(AES.new(key, AES.MODE_CBC, iv).decrypt(ciphered))
